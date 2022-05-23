95b0f3bee615410227db0da21887852cc24de409
davidbonvin <ledave.musique@gmail.com>
2022-05-23 09:27:46 +0200

    copia de mi cv

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+const article1 = 
+
+// buttons Afpa
+
+function fiche_technique(){
+    document.getElementById("Afpa_article").innerHTML =  "information"
+}
+
+
+
+// Buttons Platzi
+function fiche(){
+    document.getElementById("list_compe").innerHTML = "Cours préparatoire de développeur en ligne <br> Cours de base en génie informatique <br> Cours de front-end <br> Cours de css <br> Cours de base de javascript <br> Cours git et github <br>Cours de figma"
+}
+
+function fichess(){
+  document.getElementById("list_compe").innerHTML = "";
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Portefeuille_de_David
+Voici mon Portfolio que j'ai réalisé lors de mes cours à l'AFPA "Nurserie Numerique"
